3786f0ce6db3f5b4fe9bfecb43ba19afd871347b
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2024-03-12 22:31:02 +0100

    Servo accelerometer test

--- src/acc.py
+++ src/acc.py
@@ -2,10 +2,26 @@ import time
 import board
 import busio
 import adafruit_adxl34x
+from gpiozero import AngularServo
+import math
 
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while True:
-    print("%f %f %f"%accelerometer.acceleration)
-    time.sleep(1)
+    # Get acceleration values
+    x, y, z = accelerometer.acceleration
+
+    # Calculate the angle of tilt (pitch) in the x axis
+    pitch = math.atan(x / math.sqrt(y * y + z * z))
+
+    # Calculate the angle of tilt (roll) in the y axis
+    roll = math.atan(y / math.sqrt(x * x + z * z))
+
+    # Convert the angles from radians to degrees
+    pitch = math.degrees(pitch)
+    roll = math.degrees(roll)
+
+    print("Pitch: %f, Roll: %f" % (pitch, roll))
+    servo.angle = pitch * 2
